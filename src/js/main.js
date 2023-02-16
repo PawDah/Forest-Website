fdf5717539c6a97532acffa0ea780c3991ace499
Paweł Dahlke <dahlkepawelcontact@gmail.com>
2023-02-16 17:45:43 +0100

    added js for burger btn

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
 
+const navBtn = document.querySelector('.burger-btn');
+const nav = document.querySelector('.nav-mobile');
+const homeBtn = document.querySelector('.home');
+const aboutBtn = document.querySelector('.about');
+const offerBtn = document.querySelector('.offer');
+const contactBtn = document.querySelector('.contactUs');
+
+const handleNav = () => {
+	nav.classList.toggle('nav-mobile--active');
+};
+
+navBtn.addEventListener('click', handleNav);
+homeBtn.addEventListener('click', handleNav);
+aboutBtn.addEventListener('click', handleNav);
+offerBtn.addEventListener('click', handleNav);
+contactBtn.addEventListener('click', handleNav);
